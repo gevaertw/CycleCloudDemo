7cfa820d6c183cc1511ca39e684f68fe10f07415
gevaertw <37732965+gevaertw@users.noreply.github.com>
2024-03-26 21:35:07 +0100

    minor stuff

--- Slurm/slurmtest02.sh
+++ Slurm/slurmtest02.sh
@@ -9,21 +9,21 @@
 #SBATCH --output=testJob-%j.out
 #SBATCH --error=testJobErrors-%j.out
 #SBATCH --time=1-00:10:00
-#SBATCH --ntasks=16
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks=8
+#SBATCH --ntasks-per-node=2
 #SBATCH --nodes=4
 #SBATCH --mem-per-cpu=500M
 
 ## Other, non Slurm parameters
 ## how long do you want the job to run?
-jobruntime=6m
+jobruntime=10m
 ## How many jobs you want to launch?
 jobcount=3
 
 ## Job Steps
 ## Each sbatch line in the loop is considered a new job to slurm!
 srun echo "Start Slurm Test"
-for i in {1..16}
+for i in {1..8}
 do
   sbatch ./hpcjob01.sh $i $jobruntime
 done
