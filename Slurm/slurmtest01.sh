7f41315949de09e48a00423e292151fff91d3a5e
gevaertw <37732965+gevaertw@users.noreply.github.com>
2024-03-27 09:30:26 +0100

    Updated test scripts

--- Slurm/slurmtest01.sh
+++ Slurm/slurmtest01.sh
@@ -1,16 +1,3 @@
-## Shebang
 #!/bin/bash
-
-## Resource Request
-#SBATCH --job-name=testJob
-#SBATCH --output=testJob-%j.out
-#SBATCH --error=testJobErrors-%j.out
-#SBATCH --job-name=singlecpu
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-
-## Your script goes here
-srun echo "Start process"
-srun hostname
-srun sleep 30
-srun echo "End process"
+# Your actual job commands go here
+srun  --job-name=my_small_job --output=my_small_job_output.txt --nodes=1 --ntasks=1 --cpus-per-task=2 --time=1:00:00 ./hpcjob01.sh
