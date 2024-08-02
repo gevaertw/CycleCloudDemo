89f03775dde090a82034f402495a4bdbcea108bb
gevaertw <37732965+gevaertw@users.noreply.github.com>
2024-08-02 09:47:24 +0200

    _ git add --chmod=+x *.sh

--- Installers/Moneo/moneoInstallLinux.sh
+++ Installers/Moneo/moneoInstallLinux.sh
@@ -4,7 +4,10 @@
 # based on https://github.com/Azure/Moneo/blob/main/docs/HeadlessDeployment.md
 
 # Run this script on all nodes in the scale set, by starting it from the cyclecloud cloud init script
-# copy the config files from the NFS share and start the service
+
+# Copy the config files from the NFS share and start the service
 yes | \cp /cyclenfs/software/moneo/moneo_config.json /opt/azurehpc/tools/Moneo/moneo_config.json
+
+# Start the service
 /opt/azurehpc/tools/Moneo/linux_service/start_moneo_services.sh
 
