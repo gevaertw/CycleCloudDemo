b87752a148fc51591ad784d8f19f6cd274dfcad2
gevaertw <37732965+gevaertw@users.noreply.github.com>
2024-08-05 14:00:16 +0200

    Path error

--- Experiments/slurmtest05.sh
+++ Experiments/slurmtest05.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 RESULTFOLDER=/cyclenfs/results
 TIMESTAMP=$(date '+%Y-%m-%d_%H-%M-%S')
-TESTFOLDER=Slurmtest04-$TIMESTAMP
+
+JOBNAME=slurmtest05
+
+TESTFOLDER=$JOBNAME-$TIMESTAMP
 FULLPATH=$RESULTFOLDER/$TESTFOLDER
-JOBNAME=Prime_Parallel_$TIMESTAMP
+SLURMJOBNAME=Prime_Parallel_$TIMESTAMP
+
 PARTITIONNAME=htc
 
+
+# stuff that happens befor slurm is invloved
 mkdir $FULLPATH
 echo "Results are stored in ${FULLPATH}."
 
 
-# Job Steps
+# Stuff that slurm does
 # Each sbatch line in the loop is considered a new job to slurm!  its actualy the same as slurmtest03.sh but with subtasks
-
-
-sbatch  --job-name=$JOBNAME --output=$FULLPATH/%x_%j_out.txt --error=$FULLPATH/%x_%j_err.txt --cpus-per-task=1 --array=0-999 --partition=$PARTITIONNAME ./hpcjob05.sh
+sbatch  --job-name=$SLURMJOBNAME --output=$FULLPATH/%x_%j_out.txt --error=$FULLPATH/%x_%j_err.txt --cpus-per-task=1 --array=0-999 --partition=$PARTITIONNAME ./hpcjob05.sh
 
--- Installers/Moneo/moneoInstallLinux.sh
+++ Installers/Moneo/moneoInstallLinux.sh
@@ -4,8 +4,8 @@
 
 # Run this script on all nodes in the scale set, by starting it from the cyclecloud cloud init script.  It assumes that the azure install part, where you add the user assigned identity to the VMSS has already been done.
 
-# Copy the config files from the NFS share and start the service
-yes | \cp /cyclenfs/software/moneo/moneo_config.json /opt/azurehpc/tools/Moneo/moneo_config.json
+# Copy the config files from the NFS share and start the service.  If you have named the NFS share differently or you gip cloned in another directory, you will need to adjust the paths. 
+yes | \cp /cyclenfs/CycleCloudDemo/Installers/Moneo/moneo_config.json /opt/azurehpc/tools/Moneo/moneo_config.json
 
 # Start the service
 /opt/azurehpc/tools/Moneo/linux_service/start_moneo_services.sh
