910d0777a86a1802d66b124188b374181012b5d5
gevaertw <37732965+gevaertw@users.noreply.github.com>
2024-08-06 11:25:14 +0200

    fixed moneo

--- Installers/Moneo/moneoInstallLinux.sh
+++ Installers/Moneo/moneoInstallLinux.sh
@@ -5,8 +5,8 @@
 # Run this script on all nodes in the scale set, by starting it from the cyclecloud cloud init script.  It assumes that the azure install part, where you add the user assigned identity to the VMSS has already been done.
 
 # Copy the config files from the NFS share and start the service.  If you have named the NFS share differently or you gip cloned in another directory, you will need to adjust the paths. 
-yes | \cp /cyclenfs/CycleCloudDemo/Installers/Moneo/moneo_config.json /opt/azurehpc/tools/Moneo/moneo_config.json
+yes | \cp /cyclenfs/CycleCloudDemo/Installers/moneo/moneo_config.json /opt/azurehpc/tools/Moneo/moneo_config.json
 
-# Start the service
-/opt/azurehpc/tools/Moneo/linux_service/start_moneo_services.sh
+# Start the service delayed by 5 minutes to allow the VM to fully boot up and to avoid that cluster init will kill the service by restarting the network
+at now + 5 minutes -f /opt/azurehpc/tools/Moneo/linux_service/start_moneo_services.sh
 
