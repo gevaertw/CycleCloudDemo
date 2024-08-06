634b8598bff778c6893c807ab778c5707a5832c5
gevaertw <37732965+gevaertw@users.noreply.github.com>
2024-08-06 14:58:49 +0200

    minor Moneo installer bug

--- Installers/Moneo/moneoInstallLinux.sh
+++ Installers/Moneo/moneoInstallLinux.sh
@@ -6,8 +6,8 @@
 
 # Copy the config files from the NFS share and start the service.  If you have named the NFS share differently or you gip cloned in another directory, you will need to adjust the paths. 
 yes | \cp /cyclenfs/CycleCloudDemo/Installers/Moneo/moneo_config.json /opt/azurehpc/tools/Moneo/moneo_config.json
-yes | \cp /cyclenfs/CycleCloudDemo/Installers/Moneo/moneo@.service /etc/systemd/system
-yes | \cp /cyclenfs/CycleCloudDemo/Installers/Moneo/moneo_publisher.service /etc/systemd/system
+yes | \cp /cyclenfs/CycleCloudDemo/Installers/Moneo/moneo@.service /etc/systemd/system/moneo@.service
+yes | \cp /cyclenfs/CycleCloudDemo/Installers/Moneo/moneo_publisher.service /etc/systemd/system/moneo_publisher.service
 # Start the service delayed by 5 minutes to allow the VM to fully boot up and to avoid that cluster init will kill the service by restarting the network
 # at now + 5 minutes -f /opt/azurehpc/tools/Moneo/linux_service/start_moneo_services.sh
 
