2ef87e2d70b3728724052b73feb3a5d657a7ca23
gevaertw <37732965+gevaertw@users.noreply.github.com>
2024-08-06 16:29:43 +0200

    more moneo fixes

--- Installers/Moneo/moneoInstallLinux.sh
+++ Installers/Moneo/moneoInstallLinux.sh
@@ -8,7 +8,19 @@
 yes | \cp /cyclenfs/CycleCloudDemo/Installers/Moneo/moneo_config.json /opt/azurehpc/tools/Moneo/moneo_config.json
 yes | \cp /cyclenfs/CycleCloudDemo/Installers/Moneo/moneo@.service /etc/systemd/system/moneo@.service
 yes | \cp /cyclenfs/CycleCloudDemo/Installers/Moneo/moneo_publisher.service /etc/systemd/system/moneo_publisher.service
-# Start the service delayed by 5 minutes to allow the VM to fully boot up and to avoid that cluster init will kill the service by restarting the network
-# at now + 5 minutes -f /opt/azurehpc/tools/Moneo/linux_service/start_moneo_services.sh
 
-/opt/azurehpc/tools/Moneo/linux_service/start_moneo_services.sh
+# Start the service delayed by 5 minutes to allow the VM to fully boot up and to avoid that cluster init will kill the service by restarting the network, altough this works I don't think it's nice
+at now + 5 minutes -f /opt/azurehpc/tools/Moneo/linux_service/start_moneo_services.sh
+
+
+# fix in systemctl
+# /opt/azurehpc/tools/Moneo/linux_service/start_moneo_services.sh
+
+# systemctl enable moneo@.service
+# systemctl enable moneo_publisher.service
+
+# systemctl start  moneo@.service
+# systemctl start  moneo_publisher.service
+
+# try fix with reboot after cloud init
+
